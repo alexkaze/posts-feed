377c76c8b26b83a1c5c784ac43931a75034a4ab1
Alex <aekazak@yandex.ru>
2024-04-24 17:04:38 +0300

    Some PostPage edits

--- src/pages/PostPage/ui/PostPage.tsx
+++ src/pages/PostPage/ui/PostPage.tsx
@@ -2,18 +2,18 @@ import { useNavigate, useParams } from 'react-router-dom';
 
 import { useGetPostByIdQuery } from '../api';
 
+import { PostError } from '@/widgets/PostError';
 import { Card } from '@/shared/ui/Card';
 import { Loading } from '@/shared/ui/Loading';
 import { ButtonLink } from '@/shared/ui/ButtonLink';
 import { Post } from '@/shared/ui/Post';
-import { PostError } from '@/widgets/PostError';
 
 import styles from './PostPage.module.scss';
 
 const PostPage = () => {
   const navigate = useNavigate();
-  const { postId } = useParams();
 
+  const { postId } = useParams();
   const { data: post, error, isLoading } = useGetPostByIdQuery(+postId!);
 
   const content = (
